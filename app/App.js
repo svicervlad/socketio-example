2ecdf11adac85fc6deca0f07c88db4f1add46780
svicer <svicervlad@gmail.com>
2022-01-05 22:18:22 +0200

    feat: implement simple socker.io clien-server app

--- app/App.js
+++ app/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import io from 'socket.io-client'
+
+const SERVER_URL = 'http://localhost:5000'
 
 function App() {
+  const [message, setMessage] = useState()
+
+  const socketRef = useRef(null)
+
+  useEffect(() => {
+    socketRef.current = io(SERVER_URL)
+  }, [])
+
   return (
     <div className="App">
-      <h1>Helllo</h1>
+      <h1>Set msg</h1>
+      <input type="text" aria-label='msg' onChange={(e) => setMessage(e.target.value)}/>
+      <button
+        onClick={(e) => {
+          socketRef.current.emit('message', message)
+        }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
---
+++ server/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+
+if __name__ == "__main__":
+    socketio.run(app)
