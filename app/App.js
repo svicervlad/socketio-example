237c338994b453fb16f39a3e68a4060021551028
svicer <svicervlad@gmail.com>
2022-01-09 15:08:02 +0200

    feat: add antd fraemwork for styling

--- app/App.js
+++ app/App.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client'
+import { Button, Typography, Input, Divider, Row, Col } from 'antd';
+import "antd/dist/antd.css";
+
+const { Title, Paragraph, Text, Link } = Typography;
 
 const SERVER_URL = 'ws://localhost:5000'
 
@@ -9,20 +13,35 @@ function App() {
   const socketRef = useRef(null)
 
   useEffect(() => {
-      socketRef.current = io(SERVER_URL)
+    socketRef.current = io(SERVER_URL)
   }, [])
 
   return (
     <div className="App">
-      <h1>Set msg</h1>
-      <input type="text" aria-label='msg' onChange={(e) => setMessage(e.target.value)}/>
-      <button
-        onClick={(e) => {
-          socketRef.current.emit('message', message)
-        }}
-      >
-        Submit
-      </button>
+      <Row>
+        <Typography>
+          <Title>Set msg</Title>
+        </Typography>
+      </Row>
+      <Row>
+        <Col>
+          <Input
+            placeholder="Set new messages"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </Col>
+        <Col>
+          <Button
+            type="primary"
+            onClick={(e) => {
+              socketRef.current.emit('message', message)
+            }}
+          >
+            Submit
+          </Button>
+        </Col>
+      </Row>
+
     </div>
   );
 }
