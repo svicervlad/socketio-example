e889005f034d04d290f2ad46d18d5b38e550d7d7
svicer <svicervlad@gmail.com>
2022-01-22 15:23:17 +0000

    fix: indent

--- server/init_db.py
+++ server/init_db.py
@@ -2,10 +2,10 @@ from main import create_app
 from database import db, User
 
 if __name__ == "__main__":
-  app = create_app()
-  app.app_context().push()
+    app = create_app()
+    app.app_context().push()
 
-  db.create_all()
-  admin = User(username='admin')
-  db.session.add(admin)
-  db.session.commit()
+    db.create_all()
+    admin = User(username="admin")
+    db.session.add(admin)
+    db.session.commit()
--- server/sockets.py
+++ server/sockets.py
@@ -1,16 +1,18 @@
 from flask_socketio import SocketIO, send, emit
 from database import db, User
 
-socketio = SocketIO(cors_allowed_origins='*',logger=True, engineio_logger=True)
+socketio = SocketIO(cors_allowed_origins="*", logger=True, engineio_logger=True)
 
-@socketio.on('user create')
+
+@socketio.on("user create")
 def user_create(user_name):
     user = User(username=user_name)
     db.session.add(user)
     db.session.commit()
 
-@socketio.on('get users')
+
+@socketio.on("get users")
 def user_list():
-  users = User.query.order_by(User.id).all()
-  users = [(dict(row.serialize)) for row in users]
-  emit('users list', users, json=True, broadcast=True)
+    users = User.query.order_by(User.id).all()
+    users = [(dict(row.serialize)) for row in users]
+    emit("users list", users, json=True, broadcast=True)
